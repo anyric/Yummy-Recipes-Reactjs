a72e9ead488f4ea0b0f03e17291ab5e09fce73f2
Anyama Richard <richard@Anyamas-MacBook-Pro.local>
2018-03-13 09:50:39 +0300

    Add test and coverall 	.travis.yml - Add coverall 	src/app/components/recipes/AddRecipe.test.js - Add snapshot test 	src/app/components/recipes/__snapshots__/AddRecipe.test.js.snap - Add snapshot 	src/app/controller/AxiosInstance.js - updated baseurl

--- src/app/components/recipes/AddRecipe.test.js
+++ src/app/components/recipes/AddRecipe.test.js
@@ -3,12 +3,19 @@ import { shallow } from 'enzyme';
 
 import { AddRecipe } from './AddRecipe';
 
-describe('<AddRecipe />', () => {
+describe('AddRecipe component', () => {
   const component = shallow(<AddRecipe />);
   it('renders without crashing', () => {
     shallow(<AddRecipe />);
   });
-  it('<AddRecipe />', () => {
+  it('returns one component', () => {
     expect(component).toHaveLength(1);
   });
+  it('renders properly', () => {
+    expect(component).toMatchSnapshot();
+  });
+  it('renders divs', () => {
+    expect(component.find('div').length).toBe(7);
+  });
 });
+
